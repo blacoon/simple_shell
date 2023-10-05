2165d57a1c7cabbc664f8a53602485954d1553ac
abdulmaleekadams <harbdulmaleek.com>
2023-10-05 08:48:53 +0100

    A code for betty

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * printHelloUser - print hello {username}.
+ * @username: the username argument
+ * Return: 0 if no error
+ */
+
+int printHelloUser(username)
+{
+	printf("Hello " + username);
+	return 0;
+}
