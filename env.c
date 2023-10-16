7685d20c86ce2d9ad04ac976109a8ea60e55b2ee
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-10-16 16:08:22 +0100

    Env var print function

---
+++ env.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * env - Print the enviroment var to the standard output
+ * @cmd: An array of str representing the command and its argument
+ * @global: Pointer to the globlal env struct
+ */
+void env(char **cmd, global_t *global)
+{
+	env_t *ptr = global->env;
+
+	(void)cmd;
+	while (ptr)
+	{
+		if (ptr->name)
+		{
+			print(ptr->name, 1, 0);
+			print("=", 1, 0);
+			print(ptr->value, 1, 1);
+		}
+		ptr = ptr->next;
+	}
+	global->exit_code = 0;
+}
