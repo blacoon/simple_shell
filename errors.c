fcb10b4b6289a11b4713642dadce4b05e3ddd71d
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-10-16 16:34:55 +0100

    Add func to print err msg

--- errors.c
+++ errors.c
@@ -14,3 +14,23 @@ int print(char *str, int fd, int new)
 		return (write(fd, "\n", 1));
 	return (1);
 }
+
+/**
+ * print_error - Prints an error message to the standard error output
+ * @cmd: The name of the command where the error occurred
+ * @msg: The error message to be displayed
+ * @global: A pointer to the global state struct
+ * Return: Always return 1
+ */
+int print_error(char *cmd, char *msg, global_t *global)
+{
+	write(2, global->name, _strlen(global->name));
+	write(2, ": ", 2);
+	putnbr_fd(global->n, 2);
+	write(2, ": ", 2);
+	write(2, cmd, _strlen(cmd));
+	write(2, ": ", 2);
+	write(2, msg, _strlen(msg));
+	write(2, "\n", 1);
+	return (1);
+}
