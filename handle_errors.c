f8f55d8120d84b76b19fedb86fdeaea6af9affff
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-10-18 10:02:23 +0100

    Rename error file

---
+++ handle_errors.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * print - Writes a string to a specified file descriptor
+ * @str: The string to be written
+ * @fd: The specified file descriptor
+ * @new: A flag indicating whether to append a newline  or not
+ * Return: The numberof char written (excluding the '\n' char if written)
+ */
+int print(char *str, int fd, int new)
+{
+	write(fd, str, _strlen(str));
+	if (new)
+		return (write(fd, "\n", 1));
+	return (1);
+}
+
+/**
+ * print_error - Prints an error message to the standard error output
+ * @cmd: The name of the command where the error occurred
+ * @msg: The error message to be displayed
+ * @global: A pointer to the global state struct
+ * Return: Always return 1
+ */
+int print_error(char *cmd, char *msg, global_t *global)
+{
+	write(2, global->name, _strlen(global->name));
+	write(2, ": ", 2);
+	putnbr_fd(global->n, 2);
+	write(2, ": ", 2);
+	write(2, cmd, _strlen(cmd));
+	write(2, ": ", 2);
+	write(2, msg, _strlen(msg));
+	write(2, "\n", 1);
+	return (1);
+}
